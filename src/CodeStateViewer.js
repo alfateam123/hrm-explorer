3ad817816d731ab26a343a7be737b1f45f940911
alfateam123 <alfateam123@nwa.xyz>
2018-02-23 21:33:42 +0100

    feat(CodeStateViewer): show the whole dump, remove comments

--- src/CodeStateViewer.js
+++ src/CodeStateViewer.js
@@ -1,20 +1,7 @@
 import React, {Component} from "react";
-/*
-InternalState { register: None, 
-  input_tape: [Number { value: 5 }, Number { value: 5 }, Number { value: 5 }, Number { value: 1 }],
-output_tape: [], memory: [None, None, None, None, None, None, None, N
-None, None, None, Some(Number { value: 0 }), Some(Number { value: 4 })], instruction_counter: 1 }
-*/
 
 export class CodeStateViewer extends Component {
-/*
-	renderRegister(rawRegister) {
-		if(!rawRegister) return "None";
 
-		if(rawRegister.hasOwnProperty("Number")) return rawRegister.Number;
-		if(rawRegister.hasOwnProperty("Character")) return rawRegister.Character;
-	}
-*/
 	renderValue(rawValue){
 		if(!rawValue) return "None";
 
@@ -23,16 +10,14 @@ export class CodeStateViewer extends Component {
 	}
 
 	render() {
-
 		const state = this.props.dump.internal_state;
 		const memory = state.memory.map(memcell => <div className="memory-cell">{this.renderValue(memcell)}</div>);
+		const input_tape = state.input_tape.map(input => <li>{this.renderValue(input)}</li>);
+		const output_tape = state.output_tape.map(output => <li>{this.renderValue(output)}</li>);
 		return <div className="code-state">
 			<div>
 				<p>Input Tape</p>
-				<ul>
-					<li>Number(5)</li>
-					<li>Number(5)</li>
-				</ul>
+				<ul>{input_tape}</ul>
 			</div>
 			<div>
 				<div>Register: {this.renderValue(state.register)}</div>
@@ -41,9 +26,7 @@ export class CodeStateViewer extends Component {
 			</div>
 			<div>
 				<p>Output Tape</p>
-				<ul>
-					<li>Number(5)</li>
-				</ul>
+				<ul>{output_tape}</ul>
 			</div>
 		</div>;
 	}
